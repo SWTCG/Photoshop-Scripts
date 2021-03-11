55bef152a6ee7c0e680cbe0020f6f0a9bbe3c280
Matt Haechrel <haechrelmatt@gmail.com>
2021-03-10 21:00:39 -0600

    Added NonUnit tests

--- generate_cards/tests/TestNonUnit.py
+++ generate_cards/tests/TestNonUnit.py
@@ -20,3 +20,35 @@ class TestNonUnit(unittest.TestCase):
         )
         our_finest_hour.wrap_text()
         our_finest_hour.write_psd()
+
+    def test_mission(self):
+        extraction = NonUnit(
+            name="Extraction",
+            typeline="Mission",
+            expansion="BF",
+            side="N",
+            rarity="C",
+            image="BF073_Extraction.psd",
+            number="73",
+            cost="X",
+            game_text="Shuffle X+1 cards from your discard pile into your deck.\rPay X build points -> Meditate"
+        )
+        extraction.wrap_text()
+        extraction.write_psd()
+
+    def test_location(self):
+        endor_battlefront = NonUnit(
+            name="Endor Battlefront",
+            typeline="Location - Ground - Endor Forest Battlefield",
+            expansion="BF",
+            side="N",
+            rarity="R",
+            image="BF013_Endor_Battlefront.psd",
+            number="13",
+            cost="3",
+            game_text="Each Endor and Forest unit gets \"Pay 1 Force -> Ambush 1.\"\rEach Imperial gets Critical Hit 1 "
+                      "and Resilience 1.\rEach Rebel gets +1 power and Lucky 1.",
+            flavor_text="\"Oh, I told you it was dangerous here!\" \u2014C-3PO"
+        )
+        endor_battlefront.wrap_text()
+        endor_battlefront.write_psd()
