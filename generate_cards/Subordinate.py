9dd4bd980ebc14f9c423e0241522c266cb50110b
Matt Haechrel <haechrelmatt@gmail.com>
2021-03-10 22:04:32 -0600

    Added Subordinates

--- generate_cards/Subordinate.py
+++ generate_cards/Subordinate.py
@@ -1,12 +1,42 @@
+import os
+
+import photoshop.api as ps
+from numpy import array
+
+import generate_cards.util.photoshop
 from generate_cards.SWTCGCard import SWTCGCard
 
 
 class Subordinate(SWTCGCard):
-    def __init__(self, name, typeline, expansion, side, rarity, speed, power, health,
-                 game_text=None, flavor_text=None, version=None, ppi=600):
-        super().__init__(name, typeline, expansion, side, rarity, ppi)
+    def __init__(self, name, typeline, expansion, side, rarity, speed, power, health, image,
+                 game_text=None, flavor_text=None, version=None, icon=True, ppi=600):
+        super().__init__(name, typeline, expansion, side, rarity, image, game_text, flavor_text, version, icon, ppi)
         self.speed = speed
         self.power = power
         self.health = health
 
-        self.version = version
+    def wrap_text(self):
+        line_lengths = {
+            7: array([1265, 1200, 1165]) * self.ppi / 600,
+            6.5: array([1265, 1200, 1165]) * self.ppi / 600
+        }
+        self._wrap_text(line_lengths)
+        return None
+
+    def write_psd(self, auto_close=False, auto_quit=False):
+        app = ps.Application()
+        app.load(os.path.join(SWTCGCard.TEMPLATE_DIR, self.template))
+        doc = app.activeDocument(self.template)
+        self._write_psd(doc)
+
+        layer_dict = generate_cards.util.photoshop.get_layers(doc)
+
+        layer_dict["Speed"].textItem.contents = self.speed
+        layer_dict["Power"].textItem.contents = self.power
+        layer_dict["Health"].textItem.contents = self.health
+
+        if auto_close:
+            doc.close(ps.DialogModes.DisplayErrorDialogs)  # Close file without saving
+        if auto_quit:
+            app.quit()  # Exit Photoshop
+        return None
---
+++ generate_cards/tests/TestSubordinate.py
@@ -0,0 +1,71 @@
+import unittest
+
+from generate_cards.Subordinate import Subordinate
+
+
+class TestUnit(unittest.TestCase):
+    def test_flavor(self):
+        imperial_sentinel_messenger = Subordinate(
+            name="Imperial Sentinel Messenger",
+            typeline="Subordinate - Character - Imperial Droid",
+            expansion="BF",
+            side="D",
+            rarity="S",
+            image=None,
+            speed="20",
+            power="1",
+            health="1",
+            game_text="Upkeep: Gain 1 build point.",
+            flavor_text="\"Operation: Cinder is to begin at once.\""
+        )
+        imperial_sentinel_messenger.wrap_text()
+        imperial_sentinel_messenger.write_psd()
+
+    def test_unique(self):
+        dio_b = Subordinate(
+            name="Dio",
+            typeline="Subordinate - Character - Rebel Spy Droid",
+            expansion="BF",
+            side="L",
+            rarity="S",
+            image=None,
+            speed="70",
+            power="2",
+            health="1",
+            game_text="Stun 3",
+            version="B"
+        )
+        dio_b.wrap_text()
+        dio_b.write_psd()
+
+    def test_tap(self):
+        imperial_royal_guards = Subordinate(
+            name="Imperial Royal Guards",
+            typeline="Subordinate - Ground/Character - Imperial Bodyguard Squad",
+            expansion="BF",
+            side="D",
+            rarity="S",
+            image=None,
+            speed="30",
+            power="3",
+            health="3",
+            game_text="Armor\r[tap] -> Protect 3"
+        )
+        imperial_royal_guards.wrap_text()
+        imperial_royal_guards.write_psd()
+
+    def test_two_abilities(self):
+        rebel_soldier = Subordinate(
+            name="Rebel Soldier",
+            typeline="Subordinate - Character - Rebel Soldier",
+            expansion="BF",
+            side="L",
+            rarity="S",
+            image=None,
+            speed="40",
+            power="2",
+            health="2",
+            game_text="Accuracy 1\rFocus 1"
+        )
+        rebel_soldier.wrap_text()
+        rebel_soldier.write_psd()
