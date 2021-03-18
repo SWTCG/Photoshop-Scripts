094ae65eafa5092609d1d5aaf17a51e44ef8712e
Matt Haechrel <haechrelmatt@gmail.com>
2021-03-18 18:26:19 -0500

    Handle empty strings as None

--- generate_cards/NonUnit.py
+++ generate_cards/NonUnit.py
@@ -12,9 +12,11 @@ from generate_cards.SWTCGCard import SWTCGCard
 class NonUnit(SWTCGCard):
     def __init__(self, name, typeline, expansion, side, rarity, number, image,
                  cost=None, game_text=None, flavor_text=None, version=None, icon=True, ppi=600):
-        super().__init__(name, typeline, expansion, side, rarity, image, game_text, flavor_text, version, icon, ppi)
         if cost == "":
             cost = None
+        if version == "":
+            version = None
+        super().__init__(name, typeline, expansion, side, rarity, image, game_text, flavor_text, version, icon, ppi)
         self.number = number
         self.cost = cost
         self.version = version
